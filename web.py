2cd119a7677449d3a13d4d953dadc2687012b00d
dan1784310 <dan1784310@gmail.com>
2026-05-14 19:07:50 +0100

    web.py

--- web.py
+++ web.py
@@ -8,7 +8,7 @@ import os
 
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-REDIRECT_URI = "http://localhost:5000/callback"
+REDIRECT_URI = "https://YOUR-RENDER-URL.onrender.com/callback"
 
 DISCORD_API = "https://discord.com/api"
 
